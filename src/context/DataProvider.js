e75cd192456facd25bb337f4574a58a31f7bd851
Ahmed <abdullahejazj@gmail.com>
2023-06-29 00:00:58 +0500

    changes

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -1,30 +1,30 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
-import db from "../firebase/firebase";
 import DataContext from "./DataContext";
 
 const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
-  console.log("data: on all pages ", data);
+  console.log("data on all pages: ", data);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const q = query(
-          collection(db, "yourCollectiontest"),
-          where("website_url", "==", "https://putlockermovies.fun/")
-        );
-        const querySnapshot = await getDocs(q);
-        if (!querySnapshot.empty) {
-          const item = querySnapshot.docs[0].data();
-          setData(item);
+        const websiteUrl = window.location.hostname;
+        console.log("websiteUrl: ", "https://" + websiteUrl + "/");
+        const response = await fetch(`https://websitesapi.vercel.app/api/`);
+        if (response.ok) {
+          const item = await response.json();
+          const filteredWebsites = item.filter(
+            (website) => website.website_url === "https://" + websiteUrl + "/"
+          );
+          setData(filteredWebsites[0]);
+        } else {
+          console.error("Error fetching data:", response.status);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-
     fetchData();
   }, []);
 
--- src/modules/Ads.jsx
+++ src/modules/Ads.jsx
@@ -6,19 +6,7 @@ const Ads = () => {
     <Helmet>
       <script
         type="text/javascript"
-        src="//hrscompetepickles.com/af/20/fa/af20fa02ac66c69048e7b1084a4c98f5.js"
-      ></script>
-
-      <script
-        data-cfasync="false"
-        async
-        type="text/javascript"
-        src="//sawedhindgut.com/rxFhXdVo5KDSugZX0/43565"
-      ></script>
-
-      <script
-        data-cfasync="false"
-        src="//d2kd9y1bp4zc6.cloudfront.net/?bydkd=949007"
+        src="//beholdsalmonsituated.com/af/20/fa/af20fa02ac66c69048e7b1084a4c98f5.js"
       ></script>
     </Helmet>
   );
